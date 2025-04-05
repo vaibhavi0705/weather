501dec27cde6397fc911d53114decf651c0859d5
vaibhavi0705 <gunjalkarvaibhavi@gmail.com>
2025-04-05 23:13:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+// http://api.weatherapi.com/v1/current.json?key=3878f335c490455fb31140304250504&q=London&aqi=no
+
+
+const temperatureField = document.querySelector(".temp");
+const locationField = document.querySelector(".time_location p");
+const dateandTimeField = document.querySelector(".time_location span");
+const weatherField = document.querySelector(".condition p");
+const searchField = document.querySelector(".search_area");
+const form = document.querySelector('form');
+
+form.addEventListener('submit',searchForLocation)
+let target = `London`;
+
+const fetchResults = async (targetlocation)=>{
+    let url = `http://api.weatherapi.com/v1/current.json?key=3878f335c490455fb31140304250504&q=${targetlocation}&aqi=no`
+
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    console.log(data);
+
+    let locationName = data.location.name;
+    let time = data.location.localtime;
+
+    let temp = data.current.temp_c;
+
+    let condition = data.current.condition.text
+    updateDetails(temp , locationName , time , condition)
+}
+
+function updateDetails(temp , locationName , time , condition){
+
+
+    let splitDate = time.split('')[0]
+
+    let splitTime = time.split('')[1]
+
+    let currentDay = getDayName(new Date(splitDate).getDay())
+
+    temperatureField.innerText = temp
+    locationField.innerText = locationName
+    dateandTimeField.innerText = `${splitDate} ${currentDay} ${splitTime}`
+    conditionField.innerText = condition
+
+}
+
+
+function searchForLocation(e){
+    e.preventDefault();
+
+    target = searchField.value;
+
+    fetchResults(target);
+}
+fetchResults();
+
+
+function getDayName(number){
+    switch(number){
+        case 0:
+            return 'Sunday'
+        case 1:
+            return 'Monday'
+        case 2:
+            return 'Tuesday'
+        case 3:
+            return 'Wenesday'
+        case 4:
+            return 'thursday'
+        case 5:
+            return 'Fridayday'
+        case 6:
+            return 'Saturday'
+    }
+}
